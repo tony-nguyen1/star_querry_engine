62d941c16122892a051be4f57e20d527e531953c
tony-nguyen1 <nguyentoony@gmail.com>
2024-12-16 16:43:55 +0100

    querry

--- watdiv-mini-projet-partie-2/generate_custom_queryset.sh
+++ watdiv-mini-projet-partie-2/generate_custom_queryset.sh
@@ -1,5 +1,5 @@
 for qt in testsuite/to_be_choosen_sparql_templates/*.sparql-template;
 do
    qt2=${qt##*templates/}
-   bin/Release/watdiv -q model/wsdbm-data-model.txt ${qt} 500 1 > testsuite/to_be_choosen_sparql_templates/${qt2%.sparql-template}.queryset ;
+   bin/Release/watdiv -q model/wsdbm-data-model.txt ${qt} 1000 1 > testsuite/to_be_choosen_queries/${qt2%.sparql-template}.queryset ;
 done;
